d0af0e0613dbfa3b9a70543e4d49406c6213c166
RodrigoFroes <craftland262@gmail.com>
2024-10-08 00:46:07 -0300

    Ajustes

--- src/components/projetos.php
+++ src/components/projetos.php
@@ -1,6 +1,4 @@
 <link rel="stylesheet" href="output.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 <div class="bg-back flex justify-center flex-col items-center px-4 py-8">
     <!-- Título da Seção -->
